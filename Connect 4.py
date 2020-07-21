7aad5d74a5de573a6ecf628e5911327cd3dc1e47
Juay <gohngeejuay@yahoo.com>
2020-07-21 17:03:38 +0800

    Finished GUI from youtube

--- Connect 4.py
+++ Connect 4.py
@@ -7,6 +7,7 @@ import random
 import time
 import pygame
 import sys
+import math
 
 class Connect4Board:
     def __init__(self,rows=6, columns=7):
@@ -18,8 +19,17 @@ class Connect4Board:
 
         '''
         self.board = []
+        self.nrows = rows
+        self.ncolumns = columns
         self.piece_one  = 'x'
         self.piece_two  = 'o'
+        self.SQUARESIZE = 100    #parameters for the board
+        self.COLUMN_COUNT = self.ncolumns
+        self.ROW_COUNT = self.nrows
+        self.width = self.COLUMN_COUNT * self.SQUARESIZE
+        self.height = (self.ROW_COUNT+1) * self.SQUARESIZE
+        self.size = (self.width,self.height)
+        self.screen = pygame.display.set_mode(self.size)
 
         for row in range(rows):
             board_row = []
@@ -46,8 +56,7 @@ class Connect4Board:
         '''
     
         try:
-            nrows = len(self.board)
-            for i in range(nrows-1,-1,-1):
+            for i in range(self.nrows-1,-1,-1):
                 if self.board[i][column] == ' ':
                     self.board[i][column] = piece
                     return True
@@ -55,7 +64,7 @@ class Connect4Board:
             raise Exception("Not a valid input for column: " + str(column))         
             #When the column has already been fully occupied
         except IndexError:
-            print('Not a valid column. Valid columns: 0 to 6')
+            print('Not a valid column. Valid columns: 0 to ' + str(self.ncolumns-1))
 
 
     def has_winner(self,n_seq = 4): 
@@ -65,11 +74,8 @@ class Connect4Board:
         @return:    Position of winning sequence or False if board does not have winning sequence.
         @raises:    None 
         '''
-        rows = len(self.board)
-        columns = len(self.board[0])
-        
-        for row in range(rows):
-            for col in range(columns):
+        for row in range(self.nrows):
+            for col in range(self.ncolumns):
                 if (self.board[row][col] == self.piece_one) or (self.board[row][col] == self.piece_two):
                     
                     return self.check_piece(row,col, n_seq)
@@ -99,13 +105,12 @@ class Connect4Board:
                 c = column + dc*i
             
                 
-                if r not in range(0,len(self.board)) or c not in range(0,len(self.board[0])):    #check if within boundary of board. If not stop checking that direction and move on to next direction.
+                if r not in range(0,self.nrows) or c not in range(0,self.ncolumns):    #check if within boundary of board. If not stop checking that direction and move on to next direction.
                     found_winner = False
                     break
                  
                 else:    
-                    #print(self.board[r][c])    #for checking
-                    #print(self.board[row][column])
+                    
                     if self.board[r][c] != self.board[row][column]:    #check if the current checked cell has same cell as original point
                         found_winner = False
                         break
@@ -143,145 +148,148 @@ class Connect4Board:
         @return:    int: index of row. If column has no pieces return False
         @raises:    Exception: if column outside of boundary of board
         '''
-        if abs(column) >= len(self.board[0]):
-            raise Exception("Column does not exist in the board. Must be between 0 and " + str(len(self.board[0]-1)))
+        if abs(column) >= self.ncolumns:
+            raise Exception("Column does not exist in the board. Must be between 0 and " + str(self.ncolumns-1))
         
-        nrows = len(self.board)
-        for i in range(0,nrows,1):
+        
+        for i in range(0,self.nrows,1):
             if self.board[i][column] == self.piece_one or self.board[i][column] == self.piece_two:
                 return i
         return False
 
     def draw_board(self):
-        pass
+        for c in range(self.ncolumns):
+            for r in range(self.nrows):
+                pygame.draw.rect(self.screen, (0,0,255), (c*self.SQUARESIZE, r*self.SQUARESIZE + self.SQUARESIZE, self.SQUARESIZE, self.SQUARESIZE))    #draw rectangle
 
-
-
-# def display_html(s):
-#     ''' Display string as HTML '''
-#     display(HTML(s))
-
-# def create_board_svg(board, radius):
-#     ''' Return SVG string containing graphical representation of board '''
-
-#     rows     = len(board)
-#     columns  = len(board[0])
-#     diameter = 2*radius
-
-#     svg  = '<svg height="{}" width="{}">'.format(rows*diameter, columns*diameter)
-#     svg += '<rect width="100%" height="100%" fill="blue"/>'
-
-#     for row in range(rows):
-#         for column in range(columns):
-#             piece = board[row][column]
-#             color = PIECE_COLOR_MAP[piece]
-#             cx    = column*diameter + radius
-#             cy    = row*diameter + radius
-#             svg += '<circle cx="{}" cy="{}" r="{}" fill="{}"/>'.format(cx, cy, radius*.75, color)
-
-#     svg += '</svg>'
-
-#     return svg
-
-
-
+                if self.board[r][c] == ' ':    #if position does not have any input, draw black circle
+                    pygame.draw.circle(self.screen,  (0,0,0), (int(c*self.SQUARESIZE + self.SQUARESIZE/2), int(r*self.SQUARESIZE + self.SQUARESIZE + self.SQUARESIZE/2)), int(self.SQUARESIZE/2 - 5))
+                elif self.board[r][c] == self.piece_one:    #if contains player 1 input, draw red circle
+                    pygame.draw.circle(self.screen,  (255,0,0), (int(c*self.SQUARESIZE + self.SQUARESIZE/2), int(r*self.SQUARESIZE + self.SQUARESIZE + self.SQUARESIZE/2)), int(self.SQUARESIZE/2 - 5))
+                else:    #if contains player 2 input, draw yellow circle
+                    pygame.draw.circle(self.screen,  (255,255,0), (int(c*self.SQUARESIZE + self.SQUARESIZE/2), int(r*self.SQUARESIZE + self.SQUARESIZE + self.SQUARESIZE/2)), int(self.SQUARESIZE/2 - 5))
+        pygame.display.update() #update to show changes
 
 def main():
-    board = Connect4Board()
+    board = Connect4Board(6,7)
     game_over = False
     turn = 0
     
     #reference for GUI: https://www.youtube.com/watch?v=SDz3P_Ctm7U&t=7s
-    pygame.init()    #create the window by specifying dimensions
-    SQUARESIZE = 100
-    COLUMN_COUNT = 7
-    ROW_COUNT = 6
-    
-    width = COLUMN_COUNT * SQUARESIZE
-    height = (ROW_COUNT+1) * SQUARESIZE
-    size = (width,height)
-    screen = pygame.display.set_mode(size)
+    pygame.init()            
+    board.draw_board()    #draw the empty board based on the instance dimension
+    pygame.display.update()    #update based on draw_board function
 
-
+    myfont = pygame.font.SysFont("monospace", 75)
     #game loop
     board.print_board()
+
     while not game_over:
     
         for event in pygame.event.get():    #allow to quit by clicking exit button
-            if event.type == pygame.QUIT:
+            if event.type == pygame.QUIT:    
                 sys.exit()
-
-
-        #Player 1 input
-        if turn == 0:
-            userInput = input("Which column selected?: ")
             
-            try:
-                board.drop_piece(int(userInput),board.piece_one)
-                board.print_board()
-                #check_winner = board.has_winner()    #Might change this to check only the last inserted location instead of all the positions in board
-                
-                prevRow = board.previous_row(int(userInput))
-                check_winner = board.check_piece(prevRow,int(userInput))
-                
-                if check_winner != False: #if current turn results in winning state, end. else continue
-                    game_over = True
+            if event.type == pygame.MOUSEMOTION:    #add animation of moving the piece
+                pygame.draw.rect(board.screen, (0,0,0) , (0,0, board.width,board.SQUARESIZE))    #draw black rectangle to remove overlapping circles
+                posx = event.pos[0]
+                if turn == 0: #player 1 turn, draw red circle
+                    pygame.draw.circle(board.screen, (255,0,0), (posx, int(board.SQUARESIZE/2 )),int(board.SQUARESIZE/2 - 5))
+                else: #player 2 turn, draw red circle
+                    pygame.draw.circle(board.screen, (255,255,0), (posx, int(board.SQUARESIZE/2 )),int(board.SQUARESIZE/2 - 5))
+            pygame.display.update()
+
+
+            if event.type == pygame.MOUSEBUTTONDOWN:    #event where mouse clicked
+                pygame.draw.rect(board.screen, (0,0,0) , (0,0, board.width,board.SQUARESIZE))    #draw black rectangle to remove overlapping circles
+                #print(event.pos)
+                # #Player 1 input
+                if turn == 0:
+                    posx = event.pos[0]    #access the x position of tuple
+                    userInput = int(math.floor(posx/board.SQUARESIZE))    #first column = 0 to 100, sec col = 100 to 200, etc. 
+                    #userInput = input("Which column selected?: ")
                     
-                    # #Get the winner at position check_winner[0],check_winner[1]
-                    # player_winner = board.get_winner(check_winner[0],check_winner[1])
-                
-                    # print("Game Over. The winner is: " + player_winner)
+                    try:
+                        board.drop_piece(int(userInput),board.piece_one)
+                        board.print_board()
+                        board.draw_board()
+                        #check_winner = board.has_winner()    #Might change this to check only the last inserted location instead of all the positions in board
+                        
+                        prevRow = board.previous_row(int(userInput))
+                        check_winner = board.check_piece(prevRow,int(userInput))
+                        
+                        if check_winner != False: #if current turn results in winning state, end. else continue
+                            game_over = True
+                            
+                            # #Get the winner at position check_winner[0],check_winner[1]
+                            # player_winner = board.get_winner(check_winner[0],check_winner[1])
+                        
+                            # print("Game Over. The winner is: " + player_winner)
+                            
+                            #Leave it as player1, since anyway player2 makes a winning move this round, otherwise would have detected player1 before or earlier
+                            print("Game Over. The winner is: Player 1")
+                            
+                            label = myfont.render("Player 1 Wins!",1, (255,0,0))    #show on screen
+                            board.screen.blit(label, (40,10))
+                            pygame.display.update() #update to show changes
+                            pygame.time.wait(3000) #give 3 seconds before automatic close window
+                            
+                        else:
+                            turn = 1
+
+                    except IndexError:
+                        print("Try Again")
+                        board.print_board()
+                        continue
+
+                    except Exception:    #If didn't insert correctly
+                        print("Try Again")
+                        board.print_board()
+                        continue
+
                     
-                    #Leave it as player1, since anyway player2 makes a winning move this round, otherwise would have detected player1 before or earlier
-                    print("Game Over. The winner is: Player 1")
+                #Player 2 input
+                elif turn == 1:
+                    posx = event.pos[0]    #access the x position of tuple
+                    userInput = int(math.floor(posx/board.SQUARESIZE))    #first column = 0 to 100, sec col = 100 to 200, etc. 
+                    #userInput = input("Which column selected?: ")
                     
-                else:
-                    turn = 1
+                    try:
+                        board.drop_piece(int(userInput),board.piece_two)
+                        board.print_board()
+                        board.draw_board()
 
-            except IndexError:
-                print("Try Again")
-                board.print_board()
-                continue
+                        #check_winner = board.has_winner()    #Might change this to check only the last inserted location instead of all the positions in board
 
-            except Exception:    #If didn't insert correctly
-                print("Try Again")
-                board.print_board()
-                continue
+                        prevRow = board.previous_row(int(userInput))
+                        check_winner = board.check_piece(prevRow,int(userInput))
 
-            
-        #Player 2 input
-        elif turn == 1:
-            userInput = input("Which column selected?: ")
-            try:
-                board.drop_piece(int(userInput),board.piece_two)
-                board.print_board()
+                        if check_winner != False: #if current turn results in winning state, end. else continue
+                            game_over = True
+                            #Get the winner at position check_winner[0],check_winner[1]
+                            #player_winner = board.get_winner(check_winner[0],check_winner[1])
 
-                #check_winner = board.has_winner()    #Might change this to check only the last inserted location instead of all the positions in board
+                            #print("Game Over. The winner is: " + player_winner)    
+                            #Leave it as player2, since anyway player2 makes a winning move this round, otherwise would have detected player1 before or earlier
+                            print("Game Over. The winner is: Player 2")
 
-                prevRow = board.previous_row(int(userInput))
-                check_winner = board.check_piece(prevRow,int(userInput))
+                            label = myfont.render("Player 2 Wins!",1, (255,255,0))    #show on screen
+                            board.screen.blit(label, (40,10))
+                            pygame.display.update() #update to show changes
+                            pygame.time.wait(3000) #give 3 seconds before automatic close window
 
-                if check_winner != False: #if current turn results in winning state, end. else continue
-                    game_over = True
-                    #Get the winner at position check_winner[0],check_winner[1]
-                    #player_winner = board.get_winner(check_winner[0],check_winner[1])
+                        else:
+                            turn = 0
+                    
+                    except IndexError:
+                        print("Try Again")
+                        board.print_board()
+                        continue
 
-                    #print("Game Over. The winner is: " + player_winner)    
-                    #Leave it as player2, since anyway player2 makes a winning move this round, otherwise would have detected player1 before or earlier
-                    print("Game Over. The winner is: Player 2")
-
-                else:
-                    turn = 0
-            
-            except IndexError:
-                print("Try Again")
-                board.print_board()
-                continue
-
-            except Exception:    #If didn't insert correctly
-                print("Try Again")
-                board.print_board()
-                continue
+                    except Exception:    #If didn't insert correctly
+                        print("Try Again")
+                        board.print_board()
+                        continue
 
             
 
