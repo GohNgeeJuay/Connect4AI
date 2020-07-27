31dcdc70d538696e4ecd0ebe9d63909035cd135c
Juay <gohngeejuay@yahoo.com>
2020-07-27 21:44:41 +0800

    FInished Tree and start to fill up tree

--- AI.py
+++ AI.py
@@ -1,34 +1,55 @@
 from Tree import *
+from Connect4 import *
+import copy
 
 
 class Connect4AI():
-    def __init__(self,board,depth = 3,lastPiece):
+    def __init__(self,board,lastPiece,depth = 3):
     
+        self.board = board
         self.tree = Tree(board)
-        self.depth = 0
+        self.depth = 3
         self.lastPiece = lastPiece
+        
+        self.insert(self.tree.root)
+        
+        # #insert into individual columns
+        # for i in range(board.ncolumns):
+        #     self.insert(self.tree.root)
+        
+        
+    def grow(self, currentNode, depth = 0,length = 1):    
+        
+        c = 0    #For column 0
+        r = self.board.previous_row(0)    #Get the previous row used. If column empty use index 0 as row
+        if r == False:    
+            r = 0
+                
 
-        while self.depth < depth:
+        
+        if r not in range(0,len(currentNode.data)):    #check if within boundary of board. If not stop adding in that column.
+            return 
+        
+        winning_pos = self.board.check_piece(r,0)    #stop if the current 
+        if winning_pos != False:    
+            return
+
+               
+        
+        else:
             
+            copyBoard = copy.deepcopy(currentNode.data) #create copy of the board
+            copyBoard[r][c] = 'o' #add the new piece
+            currentNode.add_child(Node(copyBoard))    #create a new node for the child with new board as data
 
-        
-        
-    def insert(self)    
-        #Need to create the graph 
-        DIRECTIONS = (
-        (-1, -1), (-1, 0), (-1, 1),
-        ( 0, -1),          ( 0, 1),
-        ( 1, -1), ( 1, 0), ( 1, 1),
-        )
+            prevChildIndex = currentNode.get_previous_added_child()    #change the currentNode to child for recursion
+            currentNode = currentNode.child[prevChildIndex]
 
-        
-
-
-
-      
-
-
-
+            depth += 1
+            if depth >= self.depth:
+                return
+            else:
+                self.grow(currentNode, depth, length + 1)
 
     # #Minimax function reference: https://www.youtube.com/watch?v=l-hh51ncgDI&list=WL&index=32&t=0s
     # def minimax(position, depth, alpha, beta, maximisingPlayer):
@@ -36,8 +57,17 @@ class Connect4AI():
 
 
 
-x = Connect4AI([['x',' ',' ',' ',' ',' ',' '],[' ',' ',' ',' ',' ',' ',' '],[' ',' ',' ',' ',' ',' ',' '],[' ',' ',' ',' ',' ',' ',' '],[' ',' ',' ',' ',' ',' ',' ']
-,[' ',' ',' ',' ',' ',' ',' '],[' ',' ',' ',' ',' ',' ',' ']], lastPiece= [0,0])
+board = Connect4Board()
+board.drop_piece(0,'o')
+
+x = Connect4AI(board, lastPiece= (0,0))
 
 print(x.tree.root.data)
 
+# x = [['x',' ',' ',' ',' ',' ',' '],[' ',' ',' ',' ',' ',' ',' '],[' ',' ',' ',' ',' ',' ',' '],[' ',' ',' ',' ',' ',' ',' '],[' ',' ',' ',' ',' ',' ',' ']
+# ,[' ',' ',' ',' ',' ',' ',' '],[' ',' ',' ',' ',' ',' ',' ']]
+
+# y = copy.deepcopy(x)
+# print(y)
+# y[0][1] = 'o'
+# print(y)
--- Tree.py
+++ Tree.py
@@ -1,15 +1,14 @@
 class Tree():
     def __init__(self,data):
         self.root = Node(data)
-        self.root.data = data
-            
+                    
 
 class Node():
-    def __init__(self, data, size = 7):
+    def __init__(self, data, numOfChild = 7):
         self.data = data
-        self.child = [None] * size
+        self.child = [None] * numOfChild
         self.arrayCounter = 0
-        self.arraySize = size
+        self.arraySize = numOfChild
 
     def add_child(self, data):
         if self.arrayCounter >= self.arraySize:
@@ -18,13 +17,19 @@ class Node():
             self.child[self.arrayCounter] = data
             self.arrayCounter += 1
 
+    def get_previous_added_child(self):
+        if self.arrayCounter == 0:
+            raise Exception("This node has no child yet")
+        else: 
+            return self.arrayCounter - 1
 
 
 
 
-x = Tree(1)
-print(x.root.data)
-x.root.add_child(2)
-print(x.root.child)
-x.root.add_child(3)
-print(x.root.child)
+
+# x = Tree(1)
+# print(x.root.data)
+# x.root.add_child(2)
+# print(x.root.child)
+# x.root.add_child(3)
+# print(x.root.child)
