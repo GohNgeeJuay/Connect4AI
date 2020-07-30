d0f3ec62e897b32c5484b72b020d3c7603efe6ac
Juay <gohngeejuay@yahoo.com>
2020-07-30 16:56:43 +0800

    Added docstring and comments. Start to implement the scoring function in Node Class

--- AI.py
+++ AI.py
@@ -5,12 +5,15 @@ from Tree import *
 
 
 class Connect4AI():
-    def __init__(self,Connect4Object,lastPiece,depth = 3):
-    
-        self.board = Connect4Object.board    #
+    def __init__(self,Connect4Object):
+        ''' Creates a Connect4AI instance. Will initialize a tree of depth 3. Might introduce a better form of growing the tree. 
+        Variables: self.tree: Contains the tree structure of the possible decisions to make.
+        @param:     Connect4Object
+        @return:    None
+        @raises:    None
+
+        '''
         self.tree = Tree(Connect4Object.board)     #Put the array representation of board into the tree instead of the Connect4 object
-        self.depth = 3
-        self.lastPiece = lastPiece    #last move made by AI
         self.grow_width(self.tree.root, Connect4Object.piece_two)       #grow the tree
 
 
@@ -26,10 +29,25 @@ class Connect4AI():
         
         
     def grow_width(self, currentNode, piece, n_seq = 4):    
-        if currentNode.isLeaf == True:
+        ''' Grows the tree sideways (ie the 7 new possible new states for the board for the currentNode as children). 
+        If the currentNode is a leaf, will not grow the tree. 
+        1) Looks at the next possible row for each column to insert a new piece. 
+        2) Create a copy of the currentNode board and add the new piece. New board will become data for new node
+        3) Check if the new node is a leaf(terminal node) based on winning condition, if True, change the attribute isLeaf for that node
+        4) Give a score to the new node
+        
+
+        Variables: self.tree: Contains the tree structure of the possible decisions to make.
+        @param:     Connect4Object
+        @return:    None
+        @raises:    None
+
+        '''
+
+        if currentNode.isLeaf == True:    #if node is a terminal node, do not grow it further
             return
                  
-        #insert into individual columns
+        #insert into individual columns at each iteration
         for c in range(0,len(currentNode.data[0]),1):
 
             r = 0    #Get the previous row used.
@@ -52,7 +70,7 @@ class Connect4AI():
                 copyBoard[r][c] = piece  #add the new piece for AI
                 currentNode.add_child(Node(copyBoard))    #create a new node for the child with new board as data
 
-                #check if this newly added step produces a winning condition
+                #check if this newly added step produces a winning condition. Change the attribute isLeaf
                 DIRECTIONS = (
                 (-1, -1), (-1, 0), (-1, 1),
                 ( 0, -1),          ( 0, 1),
@@ -86,15 +104,14 @@ class Connect4AI():
                 
                 if found_winner == True:
                     currentNode.child[newChildIndex].isLeaf = True
+
+                #give a score to the board
+                currentNode.scoring((r,c))    #Pass in the recently added position into the function for evaluation
                 
-                
-
-
-                
+    
 
 
 
-   
 
 # #Minimax function reference: https://www.youtube.com/watch?v=l-hh51ncgDI&list=WL&index=32&t=0s    
 # Need to implement in a new function. 
@@ -112,5 +129,5 @@ board.drop_piece(1,'o')
 board.drop_piece(2,'x')
 
 
-x = Connect4AI(board, lastPiece= (2,1))
+x = Connect4AI(board)
 
--- Tree.py
+++ Tree.py
@@ -1,17 +1,45 @@
 class Tree():
     def __init__(self,data):
+        ''' Creates a Tree instance. Contains the root node of the tree
+        @param:     data. Intended to contain the array representation of Connect4Board
+        @return:    None
+        @raises:    None
+
+        '''
         self.root = Node(data)
                     
 
 class Node():
     def __init__(self, data, numOfChild = 7):
+        ''' Creates a Node instance. 
+        Variables: 
+        data.           Contains the array representation of Connect4Board
+        child.          Array containing the 7 children. Could contain None if not fully occupied.
+        arrayCounter.   int indicating the next empty space in the child array.
+        isLeaf.         Boolean indicating whether the node is a terminal node and shouldn't be grown further
+        value.          Contain the value of the board used for minmax algorithm
+
+        @param:     data. Intended to contain the array representation of Connect4Board
+        @param:     numOfChild: int for size of the array for number of children a node has. Default = 7.
+        @return:    None
+        @raises:    None
+
+        '''
+
         self.data = data
         self.child = [None] * numOfChild
         self.arrayCounter = 0
         self.arraySize = numOfChild
         self.isLeaf = False    #Attribute to terminate growing
+        self.value = 0    #value of the data(board) initialize to 0. Will change based on evaluation from scoring.
 
     def add_child(self, data):
+        ''' Adds a child into the child array.  
+        @param:     data. Intended to contain the array representation of Connect4Board
+        @return:    None
+        @raises:    Exception. If the child array fully occupied.
+
+        '''
         if self.arrayCounter >= self.arraySize:
             raise Exception("Child already has 7 child")
         else:
@@ -19,8 +47,23 @@ class Node():
             self.arrayCounter += 1
 
     def get_previous_added_child(self):
+        ''' Returns the index of previously added child in the child array.  
+        @param:     None
+        @return:    None
+        @raises:    Exception. If the child array is empty.
+
+        '''
         if self.arrayCounter == 0:
             raise Exception("This node has no child yet")
         else: 
             return self.arrayCounter - 1
 
+
+    def scoring(self, lastPiece):
+        ''' Returns the score of a board of a particular node  
+        @param:     None
+        @return:    None
+        @raises:    Exception. If the child array is empty.
+
+        '''
+        return
